58ff6fd56e0a34da15dcc44d00e2ea79fcdb8860
Jeanot <127698173+jean0t@users.noreply.github.com>
2025-05-29 20:52:57 -0300

    feat: added quickSort algorithm

---
+++ algorithms/sorting/quickSort.go
@@ -0,0 +1,33 @@
+package algorithms
+
+func QuickSort(arr []int) {
+    if len(arr) <= 1 {
+        return
+    }
+
+    sort(arr, 0, len(arr) - 1)
+}
+
+func sort(arr []int, low, high int) {
+    if low < high {
+       var p int = partition(arr, low, high)
+
+       sort(arr, low, p - 1)
+       sort(arr, p+1, high)
+    }
+}
+
+func partition(arr []int, low, high int) {
+    var pivot int = arr[high]
+    var i int = low - 1
+
+    for j := low; j < high; j++ {
+        if arr[j] <= pivot {
+            i++
+            arr[i], arr[j] = arr[j], arr[i]
+        }
+    }
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1
+}
