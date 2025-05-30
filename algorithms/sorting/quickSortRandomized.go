53378263b5640f135eb4deeec58a07ec128fb0cd
jean0t <example@example.com>
2025-05-29 22:26:57 -0300

    feat: optimized quicksort

---
+++ algorithms/sorting/quickSortRandomized.go
@@ -0,0 +1,44 @@
+package sorting
+
+import (
+    "time"
+    "math/rand"
+)
+
+func QuickSort(arr []int) {
+    if len(arr) < 2 {
+        return
+    }
+
+    rand.Seed(time.Now().UnixNano())
+    sort(arr, 0, len(arr)-1)
+}
+
+func sort(arr []int, low, high int) {
+    if low < high {
+        var p int = partition(arr, low, high)
+        
+        sort(arr, low, p-1)
+        sort(arr, p+1, high)
+    }
+}
+
+func partition(arr []int, low, high int) int {
+    var randomIndex int = rand.Intn(high + 1 - low) + low
+    arr[randomIndex], arr[high] = arr[high], arr[randomIndex]
+    
+    var pivot int = arr[high]
+    var i int = low
+
+    for j := low; j < high; j++ {
+        if arr[j] <= pivot {
+            i++
+            arr[i], arr[j] = arr[j], arr[i]
+        }
+    }
+
+    arr[i], arr[high] = arr[high], arr[i]
+    return i
+}
+
+
